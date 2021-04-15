61f452755e18666cce8b46131613213f45495ebd
Sinan Parmar <sinanparmar2@gmail.com>
2021-04-15 17:14:29 +0300

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,26 @@
+def resizer(data,csv,size):
+
+	#STEAD downsizer like code
+
+	return resized_hdf5, resized_csv
+
+def visualizer(model, input_hdf5, input_csv, layers_to_visualize):
+
+	#use EQT Paper task code
+
+	return graphs
+
+def result_metrics(test_results_csv):
+
+	#use Test2Results code 
+
+	return result_metrics_csv
+
+def compare(result_catalog_csv, model_to_compare_to='EQT'):
+
+	#use EQT Paper code
+
+	return comparison_txt
+
+
+
