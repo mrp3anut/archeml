38e51ef0571a2548e537dfdd392f3b5c1b7ae939
eccemerrin <62555970+eccemerrin@users.noreply.github.com>
2021-04-18 17:47:36 +0300

    Update trainer_package.py

--- trainer_package.py
+++ trainer_package.py
@@ -26,7 +26,6 @@ import shutil
 import multiprocessing
 from .EqT_utils import DataGenerator, _lr_schedule, cred2, PreLoadGenerator, data_reader
 from .models import mrp3anut, mrp3anut_salvation, mrp3anut_genesis, mrp3anut_vanilla, mrp3anut_lstm2, bclos, both_closed, both_open
-from .layers import _block_LSTM
 import datetime
 from tqdm import tqdm
 from tensorflow.python.util import deprecation
